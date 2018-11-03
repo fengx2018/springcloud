db24d7a74766ff09448f58282fbfad214849acca
max <3242216@qq.com>
2018-11-03 17:43:16 +0800

    服务集群

--- eureka-server/src/main/java/com/sun/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/sun/server/EurekaServerApplication.java
@@ -2,9 +2,11 @@ package com.sun.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaServerApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
